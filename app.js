7c03eab506d2f28c519c7ac711bf187b1e05d27f
Julie Nisbet <jmnisbet@gmail.com>
2021-07-06 13:31:51 -0700

    adding render function

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-// import functions and grab DOM elements
+import fruits from './fruits.js';
+import { renderFruit } from './render-fruit.js';
+console.log(fruits);
 
-// initialize state
+// PSEUDO CODE FOR RENDERING EACH FRUIT
+// grab the ul element (using document.getElementById)
+// call renderFruit on each fruit in fruits
+// append the li to the ul
+const fruitsUL = document.getElementById('fruits');
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+{/* <li>
+        <h3>Red Apple</h3>
+        <img src="./assets/apple.png" alt="Red Apple">
+        <span>$1.00</span>
+        <button>Add to Cart</button>
+    </li> 
+*/}
+for (let fruit of fruits) {
+    const fruitLI = renderFruit(fruit);
+    fruitsUL.appendChild(fruitLI);
+}
+
+
+
---
+++ render-fruit.js
@@ -0,0 +1,22 @@
+export function renderFruit(fruit){
+    const fruitLI = document.createElement('li');
+    const fruith3 = document.createElement('h3');
+    fruith3.textContent = fruit.name;
+    
+    const fruitImg = document.createElement('img');
+    fruitImg.src = `./assets/${fruit.image}`;
+    fruitImg.alt = fruit.name;
+    
+    const fruitSpan = document.createElement('span');
+    fruitSpan.textContent = `$${fruit.price}`;
+    
+    const button = document.createElement('button');
+    button.textContent = "Add to Cart";
+    
+    fruitLI.appendChild(fruith3);
+    fruitLI.appendChild(fruitImg);
+    fruitLI.appendChild(fruitSpan);
+    fruitLI.appendChild(button);
+
+    return fruitLI;
+}
